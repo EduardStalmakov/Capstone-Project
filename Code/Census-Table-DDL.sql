7a2ef705b2bee010a2e7e1059a8e3b7ff1faee15
Eduard <eduardstalmakov@gmail.com>
2022-09-27 13:48:05 -0500

    updated ddl

--- Code/Census-Table-DDL.sql
+++ Code/Census-Table-DDL.sql
@@ -4,6 +4,7 @@ drop table if exists dbo.Census;
 go 
 
 create table Census(
+    
     CensusID int primary key identity (1,1),
     HouseholdMembers int,
     InternetHome nvarchar(3),
@@ -18,3 +19,31 @@ create table Census(
 
 );
 
+
+DROP TABLE IF EXISTS LastFmUsers;
+
+-- Creating User Table --
+CREATE TABLE LastFmUsers (
+
+    UserID      INT             NOT NULL,        
+    UserName    VARCHAR(500)    NULL
+
+    
+);
+
+GO
+
+-- Creating Playlist Table --
+DROP TABLE IF EXISTS Playlist;
+
+CREATE TABLE Playlist (
+
+    PlaylistID      INT              Not NULL, 
+    PlaylistTitle   VARCHAR(600)     NULL,
+    UserID          INT              NOT NULL,
+
+    
+
+);
+
+GO
