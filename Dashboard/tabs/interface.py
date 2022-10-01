865ea77bd98c3e12b2b6bb607b51873733410500
Eduard <eduardstalmakov@gmail.com>
2022-10-01 16:24:41 -0500

    updated graphs

--- Dashboard/tabs/interface.py
+++ Dashboard/tabs/interface.py
@@ -18,16 +18,7 @@ top_5_artist = Playlist_Tracks_merged['ArtistName'].value_counts().sort_values(a
 # Page Layout
 
 layout = html.Div(children=[
-    html.H1(id='user-text', style={'text-align': 'center'}),
-    html.Div([
-
-    dcc.Graph(id = 'top-5-song', figure= px.bar(x=top_5_songs.index, y=top_5_songs.values), style={'padding':'0.8rem', 'marginTop':'1rem', 'marginLeft':'1rem', 'boxShadow': '#e3e3e3 4px 4px 2px', 'border-radius': '10px', 'backgroundColor': 'grey', 'width':'100%'}),
-    dcc.Graph(id = 'top-5-genre', figure= px.bar(x=top_5_genre.index, y=top_5_genre.values), style={'padding':'0.8rem', 'marginTop':'1rem', 'marginLeft':'1rem', 'boxShadow': '#e3e3e3 4px 4px 2px', 'border-radius': '10px', 'backgroundColor': 'grey','width':'100%' }),
-    dcc.Graph(id = 'top-5-artist', figure= px.bar(x=top_5_artist.index, y=top_5_artist.values), style={'padding':'0.8rem', 'marginTop':'1rem', 'marginLeft':'1rem', 'boxShadow': '#e3e3e3 4px 4px 2px', 'border-radius': '10px', 'backgroundColor': 'grey','width':'100%'}),
-    ],style={'display':'inline-flex','width': '100%'}),
-
-    html.Div([
-        html.Label(['Select User'],style={'font-weight': 'bold','padding':'0.8rem'}),
+    html.Label(['Select User'],style={'font-weight': 'bold','padding':'0.8rem'}),
         dcc.Dropdown(
             id = 'user',
             options = [
@@ -37,9 +28,19 @@ layout = html.Div(children=[
                 {'label': 'demo-crassy', 'value': 3053},
                 {'label': 'pogopatterson', 'value': 2352}
                  ],
-            style={'padding':'0.8rem', 'width':'300px'},
+            style={'padding':'0.8rem', 'width':'800px'},
             value= 3216
              ),
+    html.H1(id='user-text', style={'text-align': 'center'}),
+    html.Div([
+
+    dcc.Graph(id = 'top-5-song', figure= px.bar(x=top_5_songs.index, y=top_5_songs.values, title='Top Trending Songs', labels={'x':'Song','y':'Count on Playlists'}), style={'padding':'0.8rem', 'marginTop':'1rem', 'marginLeft':'1rem', 'boxShadow': '#e3e3e3 4px 4px 2px', 'border-radius': '10px', 'backgroundColor': 'grey', 'width':'100%'}),
+    dcc.Graph(id = 'top-5-genre', figure= px.bar(x=top_5_genre.index, y=top_5_genre.values, title='Top Trending Genres', labels={'x':'Genre','y':'Count on Playlists'}), style={'padding':'0.8rem', 'marginTop':'1rem', 'marginLeft':'1rem', 'boxShadow': '#e3e3e3 4px 4px 2px', 'border-radius': '10px', 'backgroundColor': 'grey','width':'100%' }),
+    dcc.Graph(id = 'top-5-artist', figure= px.bar(x=top_5_artist.index, y=top_5_artist.values, title='Top Trending Artists', labels={'x':'Artist','y':'Count on Playlists'}), style={'padding':'0.8rem', 'marginTop':'1rem', 'marginLeft':'1rem', 'boxShadow': '#e3e3e3 4px 4px 2px', 'border-radius': '10px', 'backgroundColor': 'grey','width':'100%'}),
+    ],style={'display':'inline-flex','width': '100%'}),
+
+    html.H4(['Get recommendations based on a playlist in real time!'],style={'text-align':'left'}),
+    html.Div([
         html.Label(['Select Playlist'],style={'font-weight': 'bold', 'padding':'0.8rem'}),
         html.Br(),
         dcc.Dropdown(
@@ -48,10 +49,9 @@ layout = html.Div(children=[
             style={'padding':'0.8rem', 'width':'300px'}),
     ],style={'display':'inline-flex'}),
 
-    html.Label(['Here are your reccommendations based on the selected playlist'],style={'font-weight': 'bold'}),
     # Create the table with recommended songs
     html.Div([
-        dash_table.DataTable(id='graph',style_table={'width': '100px'}, style_cell={'textAlign': 'center'})]),
+        dash_table.DataTable(id='recommendations',style_table={'width': '100px'}, style_cell={'textAlign': 'center'})]),
 
 ])
 
@@ -79,12 +79,12 @@ def select_user(value):
 
 #callback function to get the selected dropdown value and output the data from the function
 @app.callback(
-    Output('graph', 'data'),
+    Output('recommendations', 'data'),
     [Input(component_id='dropdown', component_property='value')]
 )
 
 #Function to run the cosine similarity model and retun a dictionary from the dataframe as input for the dash_table
-def select_graph(value):
+def get_recommendations(value):
 
     recommendations = content.display_recommendations(int(value))
     recommendations_slice = recommendations[['TrackName','ArtistName']]
