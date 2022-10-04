5623ac616317f7e6c91797c7cbc6f72df4affaf3
Vanessa Gleason <vgleason25@gmail.com>
2022-10-03 19:35:34 -0500

    reorganize tabs

--- Dashboard/index.py
+++ Dashboard/index.py
@@ -2,37 +2,20 @@ from dash.dependencies import Input, Output
 from dash import html, dcc 
 
 from app import app, server
-from tabs import intro, investors, demographics, interface 
+from tabs import intro, target, scenes, interface 
+from model import content
 
-
-style = {'maxWidth': '1500px', 'margin': 'auto'}
-
-tabs_styles = {
-    'height': '60px'
-}
-tab_style = {
-    'borderBottom': '1px solid #d6d6d6',
-    'padding': '6px',
-    'fontWeight': 'bold'
-}
-
-tab_selected_style = {
-    'borderTop': '1px solid #d6d6d6',
-    'borderBottom': '1px solid #d6d6d6',
-    'backgroundColor': '#6699CC',
-    'color': 'white',
-    'padding': '6px'
-}
-
+style = {'maxWidth': '1400px', 'margin': 'auto'}
 
 app.layout = html.Div([
     dcc.Markdown('# Music Streaming Song Recommender'),
-    dcc.Tabs(id='tabs', value='tab-intro', children=[
-        dcc.Tab(label='User Interface', value='tab-interface', style=tab_style, selected_style=tab_selected_style),
-        dcc.Tab(label='Intro', value='tab-intro', style=tab_style, selected_style=tab_selected_style),
-        dcc.Tab(label='Investors', value='tab-investors', style=tab_style, selected_style=tab_selected_style),
-        dcc.Tab(label='Demographics', value='tab-demographics', style=tab_style, selected_style=tab_selected_style),
-    ],style=tabs_styles),
+    dcc.Tabs(id='tabs', value='tab-interface', children=[
+        dcc.Tab(label='User Interface', value='tab-interface'),
+        dcc.Tab(label='Intro', value='tab-intro'),
+        dcc.Tab(label='Behind the Scenes', value='tab-scenes'),
+        dcc.Tab(label='Target Market', value='tab-target'),
+        
+    ]),
     html.Div(id='tabs-content'),
 ], style=style)
 
@@ -40,9 +23,9 @@ app.layout = html.Div([
               [Input('tabs', 'value')])
 def render_content(tab):
     if tab == 'tab-intro': return intro.layout
-    elif tab == 'tab-investors': return investors.layout
-    elif tab == 'tab-demographics': return demographics.layout
+    elif tab == 'tab-target': return target.layout
+    elif tab == 'tab-scenes': return scenes.layout
     elif tab == 'tab-interface': return interface.layout
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
