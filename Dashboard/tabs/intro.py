f27a4d0a4d6168631af7a32fdbd367a65eddccbd
Olivier <rochaixoli@gmail.com>
2022-10-04 19:10:50 -0400

    removed unnecessary tabs and moved data sources

--- Dashboard/tabs/intro.py
+++ Dashboard/tabs/intro.py
@@ -4,11 +4,11 @@ from dash import html, dcc
 from app import app
 
 layout = [dcc.Markdown("""
-### Intro
-'Dev10's Cohort 31 Team 4 "Land'o'Datalake" is Vanessa Gleason, Alistair Marsden, Olivier Rochaix, Eduard Stalmakov. Together we 
-have investgated the inner workings of music streaming services. A critical component to retaining user engagement is
-to expose users to more material, new songs, that they will find appealing. Therefore, we created two types of recommenders
-to increase the user experience, a content recommender and a collaborative recommender. 
+### About This Dashboard
+Dev10's Cohort 31 Team 4 "Land'o'Datalake" is Vanessa Gleason, Alistair Marsden, Olivier Rochaix, Eduard Stalmakov. Together we 
+have investigated the inner workings of music streaming services. A critical component to retaining user engagement is
+to expose users to more material, such as new songs that they will find appealing. Therefore, we created two types of recommenders
+to increase the user experience: a content recommender and a collaborative recommender. 
 
 #### User Interface
 
@@ -49,5 +49,11 @@ Information on the current market size of music streaming services and trends in
 Information about how our recommender models work and our data sets are available in the 'Behind the Scenes'
 tab.
 
-
+### Resources
+1.  Boland, D. (n.d.). Streamable Playlists with User Data. Retrieved October 4, 2022, 
+        from http://www.dcs.gla.ac.uk/~daniel/spud/
+2.  Eren Ay, Y. (2021, April). Spotify Dataset 1921-2020, 600k+ Tracks. Kaggle. Retrieved September 28, 2022, 
+        from https://www.kaggle.com/datasets/yamaerenay/spotify-dataset-19212020-600k-tracks
+3.  United States Census Bureau. (2020, June 22). Current Population Survey: Computer and Internet Use Supplement 2019. 
+        Retrieved September 22, 2022, from http://api.census.gov/data/2019/cps/internet/nov
 """)]
